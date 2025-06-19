6b462c1d9533f1f3f6f4d5245d8055bb123881c2
Cadet <126462556+Marius0004@users.noreply.github.com>
2025-06-19 16:10:38 +0200

    ajout de  nouveau type d'incident

--- src/Enum/IncidentType.php
+++ src/Enum/IncidentType.php
@@ -7,6 +7,12 @@ enum IncidentType: string
 case NUISANCE = 'nuisance';
 case DEGRADATION = 'degradation';
 case AUTRE = 'autre';
+case TRAVAUX = 'travaux';
+case ACCIDENT = 'accident';
+case EVENEMENT = 'evenement';
+case INFRASTRUCTURE = 'infrastructure';
+case SECURITE = 'securite';
+               
 }
 
 ?>
